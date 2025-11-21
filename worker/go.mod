290bd26545c7e6ed28278ad47ab8c279aaccb303
Pedro Augusto <pedro.aug2808@gmail.com>
2025-11-21 11:31:49 -0300

    feat: Collector and Worker

---
+++ worker/go.mod
@@ -0,0 +1,3 @@
+module worker/weather_app
+
+go 1.22.2
---
+++ worker/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for {
+		fmt.Println("All running well... ")
+		time.Sleep(5 * time.Second)
+	}
+}
