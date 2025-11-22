5442729a439b0f1e5a2b56420323d18257b5041e
Pedro Augusto <pedro.aug2808@gmail.com>
2025-11-22 13:58:28 -0300

    feat(worker): Receives data from Queue
    Go Worker receives data from RabbitMQ's weather_data queue and logs it

--- worker/go.mod
+++ worker/go.mod
@@ -1,3 +1,5 @@
 module worker/weather_app
 
 go 1.22.2
+
+require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
--- worker/main.go
+++ worker/main.go
@@ -2,12 +2,58 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"os"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func main() {
-	for {
-		fmt.Println("All running well... ")
-		time.Sleep(5 * time.Second)
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
 	}
+}
+
+func main() {
+	url := os.Getenv("RABBITMQ_URL")
+	fmt.Printf("URL: %s", url)
+	conn, err := amqp.Dial(url)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"weather_data", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages.")
+	<-forever
 }
