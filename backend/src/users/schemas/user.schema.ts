fed73f72221677106648b6e054c5082a20cbf356
Pedro Augusto <pedro.aug2808@gmail.com>
2025-11-28 18:48:36 -0300

    feat(backend): adds users resources
    Controller
    Service
    Schema
    Module

---
+++ backend/src/users/schemas/user.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type UserDocument = HydratedDocument<User>;
+
+/*
+ * Hello, how you doin?
+ *
+ * You might be thinking to yourself: "Wow, there are so little properties for this schema"
+ * And the reason for that is:
+ * (...drumroll)
+ * Because we don't really need much for this project's scope, just the basics for auth.
+ * That's it.
+ * */
+
+@Schema()
+export class User {
+    @Prop()
+    name: string;
+
+    @Prop({ required: true, unique: true })
+    email: string;
+
+    @Prop({ required: true })
+    password_hash: string;
+
+    @Prop({ required: true, enum: ["ADMIN", "USER"] })
+    role: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
