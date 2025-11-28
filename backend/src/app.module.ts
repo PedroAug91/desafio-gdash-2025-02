60092e7cbf4d136585400b969f1097f6dd330586
Pedro Augusto <pedro.aug2808@gmail.com>
2025-11-27 21:32:55 -0300

    deps(backend): Adds ConfigModule and MongooseModule to AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,23 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
-@Module({ imports: [], controllers: [AppController], providers: [AppService] })
+@Module({ 
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.getOrThrow<string>("MONGODB_URI"),
+            }),
+        }),
+    ], 
+    controllers: [AppController], 
+    providers: [AppService] 
+})
+
 export class AppModule {}
